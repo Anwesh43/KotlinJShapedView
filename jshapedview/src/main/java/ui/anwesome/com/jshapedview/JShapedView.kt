8579e8b8064b34ef03d11b2de6888b6fb3c53075
Anwesh43 <anweshthecool0@gmail.com>
2018-03-25 09:20:54 +0530

    adding JShapedView class

--- jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
+++ jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.jshapedview
 /**
  * Created by anweshmishra on 25/03/18.
  */
+import android.content.*
+import android.view.*
+import android.graphics.*
+class JShapedView(ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
