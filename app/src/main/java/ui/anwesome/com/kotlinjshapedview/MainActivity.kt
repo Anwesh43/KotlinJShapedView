7539201c57bf406c8d2dffddd1587845adf99c9a
Anwesh43 <anweshthecool0@gmail.com>
2018-03-25 13:06:34 +0530

    using JShapedView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinjshapedview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinjshapedview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinjshapedview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.jshapedview.JShapedView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        JShapedView.create(this)
     }
 }
