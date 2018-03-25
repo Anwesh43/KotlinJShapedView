f00f65237db07041a722131fe33a9a40be0fd9df
Anwesh43 <anweshthecool0@gmail.com>
2018-03-25 09:55:28 +0530

    adding JShaped class

--- jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
+++ jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
@@ -41,4 +41,47 @@ class JShapedView(ctx : Context) : View(ctx) {
             }
         }
     }
+    data class JShapedView (var i : Int, val state : State = State()) {
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w = canvas.width.toFloat()
+            val h = canvas.height.toFloat()
+            val r = Math.min(w,h)/10
+            val START_ANGLE = 165f
+            val SWEEP_ANGLE = 150f
+            val END_ANGLE = START_ANGLE - SWEEP_ANGLE
+            val LINE_X = r * Math.cos((END_ANGLE) * Math.PI/180).toFloat()
+            val LINE_Y = r * Math.sin(END_ANGLE * Math.PI/180).toFloat()
+            val SIZE_Y = Math.min(w,h)/3
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = r/5
+            canvas.save()
+            canvas.translate(w/2, h/2)
+            canvas.drawReverseArc(r, START_ANGLE, START_ANGLE - SWEEP_ANGLE * state.scales[0], paint)
+            canvas.drawLine(LINE_X, LINE_Y, LINE_X, LINE_Y - SIZE_Y * state.scales[1], paint)
+            canvas.restore()
+        }
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
+}
+fun Canvas.drawReverseArc(r : Float, start : Float, end : Float, paint : Paint) {
+    var  i = start
+    paint.style = Paint.Style.STROKE
+    val path = Path()
+    while (i >= end) {
+        val x = r * Math.cos(i * Math.PI/180).toFloat()
+        val y = r * Math.sin(i * Math.PI/180).toFloat()
+        if (i == start) {
+            path.moveTo(x, y)
+        }
+        else {
+            path.lineTo(x, y)
+        }
+        i--
+    }
+    drawPath(path, paint)
 }
