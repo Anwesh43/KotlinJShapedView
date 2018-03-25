92855242d1a3efa09425308291337f578d563e7d
Anwesh43 <anweshthecool0@gmail.com>
2018-03-25 09:57:21 +0530

    adding Animator class

--- jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
+++ jshapedview/src/main/java/ui/anwesome/com/jshapedview/JShapedView.kt
@@ -67,6 +67,31 @@ class JShapedView(ctx : Context) : View(ctx) {
             state.startUpdating(startcb)
         }
     }
+    data class Animator(var view : View, var animated : Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if (animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex : Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
 fun Canvas.drawReverseArc(r : Float, start : Float, end : Float, paint : Paint) {
     var  i = start
